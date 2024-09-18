7d8c2f4717ab544d68537c4f83f14e9a5839c33d
Jon Hilton <hilton.jon@gmail.com>
2024-09-18 10:23:52 +0100

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,73 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "DevScribe",
+  tagline: "Beautiful Code Snippets for your emails",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  organizationName: "DevScribe-Email", // Usually your GitHub org/user name.
+  projectName: "svelte-devscribe", // Usually your repo name.
+  presets: [
+    [
+      "docusaurus-preset-openapi",
+      /** @type {import('docusaurus-preset-openapi').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('docusaurus-preset-openapi').ThemeConfig} */
+    ({
+      navbar: {
+        title: "DevScribe",
+        logo: {
+          alt: "DevScribe Logo",
+          src: "img/DevScribeLogo.png",
+        },
+        items: [
+          { to: "/api", label: "API", position: "left" },
+          {
+            href: "https://devscribe.email",
+            label: "What is DevScribe?",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        copyright: `Copyright © ${new Date().getFullYear()} DevScribe, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
